b1737e737ac7bab940011bcc933dac8d4cf5d2c9
tonjehnord <tonjnord@gmail.com>
2024-04-04 10:52:45 +0200

    ish html struktur

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+export default function Layout() {
+    return (
+        <>
+        <header>
+            <h1>Booksearch</h1>
+        </header>
+        <main>
+            
+        </main>
+        </>
+    )
+
+}
